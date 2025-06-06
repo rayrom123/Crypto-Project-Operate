238968a19d8e4de0db6d36f4c517b74fc6bfa33f
rayrom123 <rayrom123456@gmail.com>
2025-06-06 21:39:49 +0700

    add requirement.txt

---
+++ FE/config.js
@@ -0,0 +1,4 @@
+const config = {
+  backendBaseUrl: "https://your-app-name.onrender.com", // Thay thế bằng URL Render của bạn
+  appHelperBaseUrl: "https://your-app-name.onrender.com/app-helper", // Thay thế bằng URL Render của bạn
+};
