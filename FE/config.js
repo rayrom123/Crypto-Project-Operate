b3423ab5db818edd049e604107d27ee9b9e2071e
rayrom123 <rayrom123456@gmail.com>
2025-06-07 16:13:48 +0700

    final project

--- FE/config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-  backendBaseUrl: "https://crypto-backend-mfu5.onrender.com", // Backend URL đã được deploy
-  appHelperBaseUrl: "https://crypto-app-helper.onrender.com", // URL của App Helper sau khi deploy
-};
